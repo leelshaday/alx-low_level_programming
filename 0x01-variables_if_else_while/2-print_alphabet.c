92425b9cfe23c866fc8931c96e6ad31ec4265615
lee <leemntesnot@gmail.com>
2022-03-14 11:24:50 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* this code write small letter alphabet */
+
+int main()
+{
+	char A;
+	for (A='a'; A<='z'; A++)
+		putchar(A);
+	putchar('\n');
+	return(0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char l;
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+{
+	for (l = 'A'; l<='Z'; l++)
+		putchar(l);
+}
+putchar('\n');
+	return(0);
+}
