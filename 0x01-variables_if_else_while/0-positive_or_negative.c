33ba8e583e6ce8c3c7db2be9ba5d631a78f46986
lee <leemntesnot@gmail.com>
2022-03-14 10:43:16 -0700

    first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (correct)
+ */
 int main(void)
 {
 	int n;
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>>
+#include <stdio.h>
 
 int main(void)
 
