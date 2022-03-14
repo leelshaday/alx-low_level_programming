de71595f552b5442703193af52b82e4017141d6d
lee <leemntesnot@gmail.com>
2022-03-14 15:21:40 -0700

    first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: return 1 after program execution
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n")
+
+return (0);
+
+}
+
