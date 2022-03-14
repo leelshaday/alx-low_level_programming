bc4a36743bf3c1d2824adc86a1b90bc39c271f1a
lee <leemntesnot@gmail.com>
2022-03-14 08:47:17 -0700

    first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,8 +11,6 @@
 int main(void)
 {
 	int n;
-
-<<<<<<< HEAD
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -30,26 +28,3 @@ printf("%i is negative\n", n);
 }
 return (0);
 }
-=======
-srand(time(0));
-n = rand() - RAND_MAX /2
-
-if (n==0)
-{
-printf("%i is zero\n", n);
-}
-
-else if (n<0)
-{printf("%i is negative\n" , n);
-}
-
-else
-{
-printf{"%i is positive\n" , n);
-}
-
-return (0);
-
-}
-
->>>>>>> 5c192153f43d7f324b1d9ea55fccac5ccc47fe83
