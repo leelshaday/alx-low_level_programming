d320a334c5a0de1deb4c1a965d843183ad8edace
leelshaday <leemntesnot@gmail.com>
2022-03-29 18:26:33 +0300

    first

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,12 +13,12 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (i < n)  
+	while (i < n)  /*Delacring WHILE*/
 	{
-		*(dest + i) = *(src + i);
+		*(dest + i) = *(src + i); /*add 1 position dest and src*/
 		i++;
 
-	}
+	} /*END WHILE*/
 
 
 	return (dest);
