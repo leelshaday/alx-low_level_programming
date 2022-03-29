7109668a06880b2081310bb2d3d295119a02eaf9
leelshaday <leemntesnot@gmail.com>
2022-03-29 18:10:26 +0300

    first

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,12 +12,12 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	
+	/*Delacring FOR*/
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b; 
+		*(s + i) = b; /*add 1 position s*/
 
-	} 
+	} /*END FOR*/
 
 	return (s);
-}
+}}
