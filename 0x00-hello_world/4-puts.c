5e0f2e1b3458416212fc8e1c6dab07e22c6b75d7
leelshaday <leemntesnot@gmail.com>
2022-04-14 18:26:31 +0300

    first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts("\"programming is like building a multilingual puzzle");
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
